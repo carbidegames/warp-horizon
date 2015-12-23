39593493abc0656399c3eed4ba3ed9f41e23f3e3
Layl <layl@edgeworksgames.com>
2015-12-23 04:45:18 +0100

    Add mouse movement to backend

--- libraries/warp_horizon_client/src/frontend.rs
+++ libraries/warp_horizon_client/src/frontend.rs
@@ -1,6 +1,8 @@
+use cgmath::Vector2;
 use input_state::GameButton;
 
 pub enum FrontendEvent {
     Press(GameButton),
     Release(GameButton),
+    MouseMove(Vector2<i32>)
 }
--- libraries/warp_horizon_client/src/input_state.rs
+++ libraries/warp_horizon_client/src/input_state.rs
@@ -1,13 +1,17 @@
+use cgmath::Vector2;
 use frontend::FrontendEvent;
 
+/// A type representing the current state of player input in ways that the game can understand.
 pub struct InputState {
-    keys: Vec<bool>
+    keys: Vec<bool>,
+    mouse_position: Vector2<i32>,
 }
 
 impl InputState {
     pub fn new() -> Self {
         InputState {
-            keys: vec![false; (GameButton::Max as usize)]
+            keys: vec![false; (GameButton::Max as usize)],
+            mouse_position: Vector2::new(0, 0)
         }
     }
 
@@ -19,12 +23,17 @@ impl InputState {
         self.keys[key as usize] = state;
     }
 
+    pub fn mouse_position(&mut self) -> Vector2<i32> {
+        self.mouse_position
+    }
+
     pub fn update(&mut self, events: &[FrontendEvent]) {
         let iter = events.iter();
         for e in iter {
             match e {
                 &FrontendEvent::Press(k) => self.set_key(k, true),
-                &FrontendEvent::Release(k) => self.set_key(k, false)
+                &FrontendEvent::Release(k) => self.set_key(k, false),
+                &FrontendEvent::MouseMove(pos) => self.mouse_position = pos
             }
         }
     }
@@ -43,6 +52,7 @@ enum_from_primitive! {
 
 #[cfg(test)]
 mod tests {
+    use cgmath::Vector2;
     use input_state::{InputState, GameButton};
     use frontend::{FrontendEvent};
 
@@ -55,7 +65,7 @@ mod tests {
     }
 
     #[test]
-    fn key_returns_state_after_update_receives_frontend_events() {
+    fn key_returns_state_after_update_receives_frontend_event() {
         let mut input_state = InputState::new();
 
         input_state.update(&vec!(FrontendEvent::Press(GameButton::MoveCameraRight)));
@@ -63,5 +73,12 @@ mod tests {
 
         input_state.update(&vec!(FrontendEvent::Release(GameButton::MoveCameraRight)));
         assert!(!input_state.key(GameButton::MoveCameraRight));
+    }
+
+    #[test]
+    fn mouse_position_returns_position_after_update_receives_frontend_event() {
+        let mut input_state = InputState::new();
+        input_state.update(&vec!(FrontendEvent::MouseMove(Vector2::new(50, 23))));
+        assert_eq!(input_state.mouse_position(), Vector2::new(50, 23));
     }
 }
